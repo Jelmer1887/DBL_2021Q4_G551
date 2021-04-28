36f13ce7fd60d23316f235592f29b80fd1e1a276
andreipreda2001 <andreipreda2001@yahoo.com>
2021-04-28 21:19:13 +0200

    Small work on Vis page

--- WebsiteExperiment/src/app/app-routing.module.ts
+++ WebsiteExperiment/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { VisComponent } from './vis/vis.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,13 @@ const routes: Routes = [
   {
     path : 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'vis',
+    component: VisComponent
   }
+
+
 ];
 
 @NgModule({
--- WebsiteExperiment/src/app/header/header.component.ts
+++ WebsiteExperiment/src/app/header/header.component.ts
@@ -14,8 +14,8 @@ import { Component, OnInit } from '@angular/core';
     <div class="navbar-menu">
       <div class="navbar-start">
         <a class="navbar-item" routerLink="/">Home</a>
+        <a class="navbar-item" routerLink="/vis">Visualisations</a>
         <a class="navbar-item" routerLink="/contact">Contact</a>
-        <a class="navbar-item" routerLink="/users">Users</a>
       </div>
     </div>
   </div>
