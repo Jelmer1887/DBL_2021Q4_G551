e0f320f6380f618b671ba2a56fa5058d9a34cf76
Jelmer <66646667+Jelmer1887@users.noreply.github.com>
2021-05-04 00:48:36 +0200

    Basic Static Visualisation page done
    Issue about no home page on connection is still  a thing, I also kinda want to install bulma extensions for fancy inputs

--- group51website/src/app/app-routing.module.ts
+++ group51website/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';   //I hoped to be able to route back to the root... Didn't work yet
+//import { AppComponent } from './app.component';   //I hoped to be able to route back to the root... Didn't work yet
 import { VisualisationPageComponent } from './visualisation-page/visualisation-page.component';
 
 const routes: Routes = [
