7c0cd119a87c27f8e63c2373ee24b165ac7d6925
Jelmer <66646667+Jelmer1887@users.noreply.github.com>
2021-05-03 16:59:16 +0200

    made the navbar
    Also includes code for the upload button prompt, however this is not sure to work in the future (also prompt not implemented yet)

--- group51website/src/app/nav-bar/nav-bar.component.ts
+++ group51website/src/app/nav-bar/nav-bar.component.ts
@@ -10,6 +10,23 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    // Link UPLOAD button to Modal
+    // as from: https://github.com/jgthms/bulma/issues/683 
+    document.querySelector('a#open-modal').addEventListener('click', function(event) {
+      event.preventDefault();
+      var modal = document.querySelector('.modal');  // assuming you have only 1
+      var html = document.querySelector('html');
+      modal.classList.add('is-active');
+      html.classList.add('is-clipped');
+    
+      modal.querySelector('.modal-background').addEventListener('click', function(e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+      });
+    });
 
+
+  }
+  
 }
