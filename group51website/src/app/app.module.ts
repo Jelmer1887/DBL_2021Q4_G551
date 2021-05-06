4ef400050478abe745f89f20607667daf6ff8e77
andreipreda2001 <andreipreda2001@yahoo.com>
2021-05-06 22:53:33 +0200

    A beginning of the homepage

--- group51website/src/app/app.module.ts
+++ group51website/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BarchartComponent } from './barchart/barchart.component';
-import { ForceGraphComponent } from './force-graph/force-graph.component'
+import { ForceGraphComponent } from './force-graph/force-graph.component';
+import { HomePageComponent } from './home-page/home-page.component'
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ForceGraphComponent } from './force-graph/force-graph.component'
     NavBarComponent,
     
     BarchartComponent,
-    ForceGraphComponent
+    ForceGraphComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
