1817e59fba4aa30a49c1d5a3b7e9797abdfc1020
MauritsCampman <m.c.campman.klophaus@student.tue.nl>
2021-05-20 23:57:25 +0200

    Arc diagram with colors and stuff
    Added colors on nodes and arcs, added better detection of "mouseon" hover event

--- group51website/src/arc-diagram/arc-diagram.component.ts
+++ group51website/src/arc-diagram/arc-diagram.component.ts
@@ -88,7 +88,7 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                 // Convert the source and target to an integer.
                 var source = parseInt(columns[1]);
                 var target = parseInt(columns[4]);
-
+                var address = columns[2];
                 // Add the source if we can't find it in the array of nodes.
                 var srcFound = false;
                 for (var n of this.nodes) {
@@ -98,7 +98,7 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                     }
                 }
                 if (!srcFound) {
-                    this.nodes.push({ "id": source, "job": columns[3] });
+                    this.nodes.push({ "id": source, "address": address ,"job": columns[3] });
                 }
 
                 // Add the target if we can't find it in the array of nodes.
@@ -110,7 +110,7 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                     }
                 }
                 if (!tarFound) {
-                    this.nodes.push({ "id": target, "job": columns[6] });
+                    this.nodes.push({ "id": target, "address": address, "job": columns[6] });
                 }
 
                 // Create the link between the source and target
@@ -145,9 +145,11 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
     }
     
     runDiagram(links, nodes, mLinkNum): void{
-        sortNodes();
+        sortNodesID(); 
         nodes.shift();//remove NaNs.
         nodes.shift();
+        //sortNodesJob();
+        console.log(nodes);
         const svg = d3.select("#arc-diagram")
             //.append("svg")
             .attr("width", this.width)
@@ -157,7 +159,21 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
         
         svg.selectAll("g").remove();        
         
-        function sortNodes() {
+        var jobs = ["CEO", "President", "Vice President", "Managing Director", "Director", "Manager", "In House Lawyer", "Trader", 
+        "Employee", "Unknown"];
+
+        //CEO, President, Vice President, Managing Director, Director, In House Lawyer, Trader, Employee, Unknown.
+        function sortNodesJob() {
+            nodes.sort(function (a,b) {
+                if (a.job == b.job) {
+                    return a.id.localCompare(b.id);
+                } else { //broky
+                        return jobs.indexOf(a.job) - jobs.indexOf(b.job); //either positive or negative, sorted accordingly
+                }
+            })
+        }
+
+        function sortNodesID() {
             nodes.sort(function(a,b) {
             if (a.id > b.id) {
                 return 1;
@@ -207,6 +223,45 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
 }
 
 `);
+    function nodeColor(job): string {
+        switch (job) {
+            case "Employee":
+                return "#68e256";
+            case "Vice President":
+                return "#56e2cf";
+            case "Manager":
+                return "#56aee2";
+            case "In House Lawyer":
+                return "#5668e2";
+            case "Trader":
+                return "#cf56e2";
+            case "Director":
+                return "#e25668";
+            case "Managing Director":
+                return "#e28956";
+            case "President":
+                return "#e2cf56";
+            case "CEO":
+                return "#8a56e2"
+            case "Unknown":
+                return "#555555";
+
+            default:
+                console.log(job);
+                return "#000000";
+        }
+    }
+//This is the ugliest solution ever. It makes the text boxes longer, making it easier to interact with them with mouse-events
+        function makeText(d){
+            if (d.id.length == 3) {
+                return " \u00A0" + d.id + "\u00A0 \u00A0 \u00A0"; //\u00A0 is unicode for NO-BREAK SPACE. HTML will ignore " "...
+            } else if (d.id.length == 2) {
+                return " \u00A0 \u00A0 \u00A0 \u00A0" + d.id + "\u00A0\u00A0\u00A0";
+            } else {
+                return " \u00A0 \u00A0 \u00A0 \u00A0 \u00A0" + d.id + "\u00A0\u00A0\u00A0";
+            }
+        }
+        console.log("  ".length);
         const node = svg.selectAll("mynodes")
             .data(nodes)
             .enter()
@@ -214,9 +269,16 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                 .attr("cy", function(d) {return(x(d.id))})
                 .attr("cx", 30)
                 .attr("r", nodeRadius)
-                .style("fill", "#69b3a2");
-        //function(d){ return(x(d.name))}
-        //add labels for nodes 
+                .style("fill", d => nodeColor(d.job));
+            //function(d){ return(x(d.name))}
+            //add labels for nodes 
+        node.append("title")
+        .text((d: any) => {
+            return "id: " + d.id + "\n" +
+                "e-mail: " + d.address + "\n" +
+                "function: " + d.job;
+            });
+
         const label = svg.selectAll("mylabels")
             .data(nodes)
             .enter()
@@ -225,27 +287,48 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                 .attr("font-family", "sans-serif")
                 .attr("x", 12)
                 .attr("transform", d => `translate(${0},${d.y = x(d.id) + nodeRadius+1})`)
-            .text(function(d){ return(d.id)})
-            .style("text-anchor", "end")
+            .text(d => makeText(d))
+            .style("text-anchor", "middle")
             //creating rectangles would make this event handling a lot more consistent, now you really have to aim your mouse to hit the text
             .on("mouseover", function(event,d) {
                 label.style('fill', "#ccc")
-                d3.select(this).style('fill', '#69b3b2')
-                link.style('stroke', a=> a.source === d.id || a.target === d.id ? '#69b3b2' : '#ccc')   
+                d3.select(this).style('font-weight', '')
+                link.style('stroke', a=> a.source === d.id || a.target === d.id ? nodeColor(d.job) : '#ccc')   
                 .style('stroke-width', a=>a.source === d.id || a.target === d.id ? 2 : 1)
             })
             .on("mouseout", function(event, d) {
                 label.style('fill', "#000")
                 d3.select(this).style('fill', '#000')
-                link.style('stroke', "#000")
+                link.style('stroke', a=> a.sentiment < -0.1 ? "#EE5555" : a.sentiment > 0.1? "#55EE55" : "#999999") 
                 .style('stroke-width', 1)                                         
             }) 
                 .call(mylabels => mylabels.append("text")
-                .attr("x", -6)
+                .attr("x", 0)
                 .attr("dy", "0.35em")
                 .attr("fill", "#008000") //d3.lab(color(d.group)).darker(2)
-                .text(d => d.id));
-        
+                .text(d => makeText(d)));
+            label.append("title")
+            .text((d: any) => {
+                return "id: " + d.id + "\n" +
+                    "e-mail: " + d.address + "\n" +
+                    "function: " + d.job;
+                });
+        /*const overlay = svg.append("g")
+            .attr("fill", " none")
+            .attr("pointer-events", "all")
+            .selectAll('rect')
+            .data(nodes)
+            .join('rect')
+                .attr("width", 23)
+                .attr("height", 3)
+                .attr('y', d=>x(d.id) - 1.5)
+                .on("mouseover", function(event,d) {
+                    label.style('fill', "#ccc")
+                    d3.selectAll("mylabels").select(label).style('fill', '#69b3b2')
+                    link.style('stroke', a=> a.source === d.id || a.target === d.id ? '#69b3b2' : '#ccc')   
+                    .style('stroke-width', a=>a.source === d.id || a.target === d.id ? 2 : 1)
+                })
+                */
         //add the links
         const link = svg.selectAll('mylinks')
             .data(links)
@@ -263,7 +346,7 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
                 .join(' ');
             })
             .style("fill", "none")
-            .attr("stroke", "black");
+            .attr("stroke", d=> this.linkColor(d.sentiment));
     }
     ngAfterViewInit(): void {
         this.width = this.container.nativeElement.offsetWidth;
@@ -271,6 +354,23 @@ export class ArcDiagramComponent implements AfterViewInit, OnChanges {
     }
     @ViewChild('container')
     container: ElementRef;
+    
+    
+    //link colour based on sentiment of message
+    linkColor(sentiment): string {
+        // console.log(sentiment);
+        for (var s of sentiment) {
+            if (s > 0.1) {
+                return "#55EE55";
+            }
+
+            if (s < -0.1) {
+                return "#EE5555";
+            }
+        }
+
+        return "#999999";
+    }
 }
 
 
