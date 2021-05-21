11914937e25e83f489dedc35dcb23d6cb9756baa
andreipreda2001 <andreipreda2001@yahoo.com>
2021-05-21 12:12:46 +0200

    Angular DOM experimentation

--- group51website/src/app/dynamic-vis-page/dynamic-vis-page.component.ts
+++ group51website/src/app/dynamic-vis-page/dynamic-vis-page.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-vis-page',
   templateUrl: './dynamic-vis-page.component.html',
   styleUrls: ['./dynamic-vis-page.component.css']
 })
-export class DynamicVisPageComponent implements OnInit {
+export class DynamicVisPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('para1') p1;
 
   constructor() {
     var slider = document.getElementById("myRange");
@@ -16,4 +17,9 @@ export class DynamicVisPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+      console.log(this.p1.nativeElement);
+  }
+
 }
+
