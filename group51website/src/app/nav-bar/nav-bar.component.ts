a5a9ab1c34ebde37cbc21bbfac82b3a9fa214a2f
Jelmer <66646667+Jelmer1887@users.noreply.github.com>
2021-06-04 02:00:57 +0200

    upload button pop-up
    still need to write code to show actual files in table;
    Issue with upload button: it doesn't actually upload...

--- group51website/src/app/nav-bar/nav-bar.component.ts
+++ group51website/src/app/nav-bar/nav-bar.component.ts
@@ -9,31 +9,51 @@ import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
 
     file;
+    file_list: File[];
+
+    modal_element: HTMLElement;
+    modal_background: HTMLElement;
 
     @ViewChild('fileInput', { static: false }) fileInput: ElementRef;
 
     constructor(private uploadService: UploadService) { }
     ngOnInit(): void {
         this.uploadService.currentFile.subscribe(newfile => this.file = newfile);
+        this.modal_background = document.getElementById('upload_modal_background');
+        this.modal_element    = document.getElementById('upload_modal');
+
+        this.modal_background.addEventListener('click', () => {
+            if (this.modal_element.className == "modal is-active"){
+                this.modal_element.className = "modal"
+            }
+        })
     }
 
     onFileUpload() {
         // get file from button
         const file = this.fileInput.nativeElement.files[0];
-        console.log(file);
         if (file.type.split('/')[1] != 'vnd.ms-excel') {
             alert('Please upload a csv file :D');
         }
 
-        this.file = file;  // update varaible to service
-        // console.log("NavBar: updated file to object: ");
-        // console.log(file);
+        this.file = file;
         this.uploadService.changeFile(this.file);
 
         // update displayed name
-        //const fileName = document.querySelector('#file-upload .file-name');
         var labels = document.querySelectorAll('#file-upload .file-name');
         labels.forEach((label) => { label.textContent = file.name; console.log(label); })
         //fileName.textContent = file.name;
+    }
+
+    // checks if the modal is shown, and inverts visibility
+    toggleUploadModal(): void {
+        if (this.modal_element.className == "modal is-active"){
+            this.modal_element.className = "modal"
+        } else {
+            this.modal_element.className = "modal is-active"
+
+            
+
+        }
     }
 }
