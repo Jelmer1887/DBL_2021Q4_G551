4d3a4ead71ca1575825b9cfa8185a121206d3980
Jelmer <66646667+Jelmer1887@users.noreply.github.com>
2021-06-27 19:04:54 +0200

    slider, logo and navbar updates part 2
    improved coloring and spacing of navbar and slider elements, worked around the logo by rendering an excessively wide version of the image, made navbar work on mobile.

--- group51website/src/app/nav-bar/nav-bar.component.ts
+++ group51website/src/app/nav-bar/nav-bar.component.ts
@@ -27,6 +27,8 @@ export class NavBarComponent implements OnInit {
                 this.modal_element.className = "modal"
             }
         })
+
+        this.addToggleNavbarBurger();
     }
 
     onFileUpload() {
@@ -58,5 +60,31 @@ export class NavBarComponent implements OnInit {
     }
     removeNotification(id:string):void{
         document.getElementById(id).className += " is-hidden";
+    }
+
+    // provided by bulma documentation: https://bulma.io/documentation/components/navbar/ 
+    addToggleNavbarBurger(): void {
+        document.addEventListener('DOMContentLoaded', () => {
+
+            // Get all "navbar-burger" elements
+            const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+          
+            // Check if there are any navbar burgers
+            if ($navbarBurgers.length > 0) {
+          
+                // Add a click event on each of them
+                $navbarBurgers.forEach( el => {
+                    el.addEventListener('click', () => {
+                        // Get the target from the "data-target" attribute
+                        const target = el.dataset.target;
+                        const $target = document.getElementById(target);
+                
+                        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                        el.classList.toggle('is-active');
+                        $target.classList.toggle('is-active');
+                    });
+                });
+            }
+        });
     }
 }
